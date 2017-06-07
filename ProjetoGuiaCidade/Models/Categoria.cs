3bd1c5a39b06fc62a0a064c38940199a5bb311f5
mrguilher621 <mrguilher621@gmail.com>
2017-06-07 18:12:02 -0300

    implementando a classe categoria

---
+++ ProjetoGuiaCidade/Models/Categoria.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjetoGuiaCidade.Models
+{
+    public class Categoria
+    {
+        public long CategoriaId { get; set; }
+        public string Titulo_Categoria { get; set; }
+    }
+}
